0c9c0004cb4f77914f9cfb101c3d77616950dfa7
butchlendio <butchlendio12@gmail.com>
2021-07-14 19:31:43 +0700

    refactor: delete  unnecessary comments

--- source/test/products/products.spec.ts
+++ source/test/products/products.spec.ts
@@ -3,7 +3,6 @@ import {expect} from 'chai'
 import {commerce,finance} from "faker"
 import Server from '../../server'
 import Products from "../../models/products"
-import Users from "../../models/users"
 
 afterEach(() => {
     Products.deleteMany({name:randomItem}).exec()
@@ -50,26 +49,4 @@ describe("Product Test", ()=>{
          expect(res.status).to.equal(200)  
     })
 
-    // it("Add new user - POST/users", async() =>{
-    //     presentUsername=randomUsername
-    //     presentPassword=randomPassword
-    //     const res = await Request(Server).post("/users")
-    //     .send({username:randomUsername,password:randomPassword})
-    //     .set({ Authorization:`Bearer ${token}`})
-    //     expect(res.status).to.equal(200)
-    // })
-
-    // it("Without token - POST/users", async() =>{
-    //     presentUsername=randomUsername
-    //     presentPassword=randomPassword
-    //     const res = await Request(Server).post("/users")
-    //     .send({username:randomUsername,password:randomPassword})
-    //     expect(res.status).to.equal(400)
-    // })
-
-    // it("User already exist - POST/users", async() =>{
-    //    const res = await Request(Server).post("/users")
-    //     .send({username:presentUsername,password:presentPassword})
-    //     expect(res.status).to.equal(400)
-    // })
 })
