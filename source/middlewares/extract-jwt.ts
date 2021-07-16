80a5796656e42f1b0ff7f32df63da24343bb5119
butchlendio <butchlendio12@gmail.com>
2021-07-16 12:31:39 +0700

    feat: add authentication

---
+++ source/middlewares/extract-jwt.ts
@@ -0,0 +1,35 @@
+import Jwt from 'jsonwebtoken';
+import { Context } from 'koa';
+import Config from '../configs/config';
+
+
+export async function  verifyToken(
+  ctx: Context,
+  next
+) {
+
+  const bearer = ctx.get('Authorization').split(' ');
+
+  let allowed = true;
+  let decoded;
+
+  const token = bearer[1];
+
+  if (bearer[0] !== 'Bearer') {
+    allowed = false;
+  }
+
+  if (!token) {
+    allowed = false;
+  }
+  try {
+    decoded = Jwt.verify(token, Config.token.secret);
+  } catch (e) {
+    ctx.status=400
+    ctx.body='Session expired'
+    allowed = false;
+    return
+  }
+await next()  
+}
+
--- source/routes/products.ts
+++ source/routes/products.ts
@@ -1,9 +1,10 @@
 import Router from 'koa-router'
 import Controller from '../controllers/products';
+import {verifyToken} from '../middlewares/extract-jwt'
 
 const router = new Router();
 
-router.get('/products', Controller.getAllProducts);
+router.get('/products',verifyToken, Controller.getAllProducts);
 
 export = router;
  
