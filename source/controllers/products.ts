20d97a22984178900b2f366a191179012961632a
butchlendio <butchlendio12@gmail.com>
2021-07-19 14:49:06 +0800

    chore: applied recomended code

--- source/controllers/products.ts
+++ source/controllers/products.ts
@@ -2,21 +2,18 @@ import Products from '../models/products';
 
 
 export const deleteProduct = async(ctx,next) =>{
-    const id =ctx.params.id
-    const decodedUsername= ctx.userName
-
-    const findproduct = await Products.findById(id)
+    const findproduct = await Products.findById(ctx.params.id)
     
     if(!findproduct){
         ctx.throw(400,"Product does not exist")
         return
     } 
     
-    if(findproduct.createdBy !== decodedUsername){
+    if(findproduct.createdBy !== ctx.userName){
         ctx.throw(400,"Not the owener of the product")
     }
 
-    const execute = await Products.findByIdAndDelete(id)
+    const execute = await Products.deleteOne({id:ctx.params.id})
 
     if(execute){
         ctx.status=200
