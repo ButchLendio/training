e3f71c9b671a8c25a87af67418a15ae1375aa92e
ButchLendio <85335914+ButchLendio@users.noreply.github.com>
2021-07-21 17:15:43 +0800

    feat: update product (#11)
    Added update product endpoint and modify existing test cases

--- source/routes/products.ts
+++ source/routes/products.ts
@@ -1,10 +1,12 @@
 import Router from 'koa-router'
-import {deleteProduct,addProduct} from '../controllers/products';
+import {updateProduct,addProduct,deleteProduct} from '../controllers/products';
 import { verifyToken } from '../middlewares/extract-jwt';
 
 const router = new Router();
 
 router.post('/products', verifyToken, addProduct)
+
+router.patch('/products/:id', verifyToken,updateProduct);
 
 router.delete('/products/:id', verifyToken,deleteProduct);
 
